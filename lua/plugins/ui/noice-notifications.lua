d6e59be8a015b707de2d62d09852f010269f4af5
sigsom129 <ssomsavath@gmail.com>
2026-03-28 03:39:09 -0400

    updated noice file

--- lua/plugins/ui/noice-notifications.lua
+++ lua/plugins/ui/noice-notifications.lua
@@ -15,13 +15,14 @@ return {
       long_message_to_split = true,
       lsp_doc_border = true,
     },
+    format = {
+      date = {
+        format = "%I:%M:%S %p",  -- 12-hour format (e.g., 02:30:45 PM)
+      },
+    },
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
   },
   keys = {
